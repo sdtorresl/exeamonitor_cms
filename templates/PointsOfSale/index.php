b93febb5168a4f9bc37ddd8d34f05f8eab49b10c
Sergio Torres <sdtorresl@unal.edu.co>
2020-02-03 22:52:03 -0500

    Index fixes

--- templates/PointsOfSale/index.php
+++ templates/PointsOfSale/index.php
@@ -18,18 +18,18 @@
         <table class="centered responsive-table">
             <thead>
                 <tr>
-                        <th scope="col"><?= $this->Paginator->sort('id') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('name') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('phone') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('contact') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('address') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('country_id') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('city_id') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('last_access') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('created') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('modified') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('customer_id') ?></th>
-                        <th scope="col" class="actions"><?= __('Actions') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('id') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('name') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('phone') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('contact') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('address') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('country_id') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('city_id') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('last_access') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('created') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('modified') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('customer_id') ?></th>
+                    <th scope="col" class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
             <tbody>
@@ -57,7 +57,7 @@
             </tbody>
         </table>
         
-        <div class="paginator">
+        <div class="paginator center-align">
             <ul class="pagination">
                 <?= $this->Paginator->first('<<') ?>
                 <?= $this->Paginator->prev('<') ?>
--- templates/Users/index.php
+++ templates/Users/index.php
@@ -18,36 +18,36 @@
         <table class="centered responsive-table">
             <thead>
                 <tr>
-                        <th scope="col"><?= $this->Paginator->sort('id') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('username') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('password') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('first_name') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('last_name') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('email') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('role') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('enabled') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('last_access') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('point_of_sale_id') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('created') ?></th>
-                        <th scope="col"><?= $this->Paginator->sort('modified') ?></th>
-                        <th scope="col" class="actions"><?= __('Actions') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('id') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('username') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('password') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('first_name') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('last_name') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('email') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('role') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('enabled') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('last_access') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('point_of_sale_id') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('created') ?></th>
+                    <th scope="col"><?= $this->Paginator->sort('modified') ?></th>
+                    <th scope="col" class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($users as $user): ?>
                 <tr>
-                                                <td><?= $this->Number->format($user->id) ?></td>
-                                                            <td><?= h($user->username) ?></td>
-                                                            <td><?= h($user->password) ?></td>
-                                                            <td><?= h($user->first_name) ?></td>
-                                                            <td><?= h($user->last_name) ?></td>
-                                                            <td><?= h($user->email) ?></td>
-                                                            <td><?= h($user->role) ?></td>
-                                                            <td><?= h($user->enabled) ?></td>
-                                                            <td><?= h($user->last_access) ?></td>
-                                                            <td><?= $this->Number->format($user->point_of_sale_id) ?></td>
-                                                            <td><?= h($user->created) ?></td>
-                                                            <td><?= h($user->modified) ?></td>
+                                                                                <td><?= $this->Number->format($user->id) ?></td>
+                                                                                            <td><?= h($user->username) ?></td>
+                                                                                            <td><?= h($user->password) ?></td>
+                                                                                            <td><?= h($user->first_name) ?></td>
+                                                                                            <td><?= h($user->last_name) ?></td>
+                                                                                            <td><?= h($user->email) ?></td>
+                                                                                            <td><?= h($user->role) ?></td>
+                                                                                            <td><?= h($user->enabled) ?></td>
+                                                                                            <td><?= h($user->last_access) ?></td>
+                                                                            <td><?= $user->has('points_of_sale') ? $this->Html->link($user->points_of_sale->name, ['controller' => 'PointsOfSale', 'action' => 'view', $user->points_of_sale->id]) : '' ?></td>
+                                                                                                            <td><?= h($user->created) ?></td>
+                                                                                            <td><?= h($user->modified) ?></td>
             
                     <td class="actions">
                         <?= $this->Html->link(__('View'), ['action' => 'view', $user->id]) ?>
@@ -59,7 +59,7 @@
             </tbody>
         </table>
         
-        <div class="paginator">
+        <div class="paginator center-align">
             <ul class="pagination">
                 <?= $this->Paginator->first('<<') ?>
                 <?= $this->Paginator->prev('<') ?>
