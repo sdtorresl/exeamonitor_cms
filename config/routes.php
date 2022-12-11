cd3a9eca29a313c12183c5eec0ece1efbbfe6967
Sergio Torres <sdtorresl@unal.edu.co>
2022-12-10 22:53:28 -0500

    Update to CakePHP 4.4

--- config/routes.php
+++ config/routes.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Routes configuration.
  *
@@ -42,6 +43,7 @@ use Cake\Routing\RouteBuilder;
  * inconsistently cased URLs when used with `:plugin`, `:controller` and
  * `:action` markers.
  */
+
 /** @var \Cake\Routing\RouteBuilder $routes */
 $routes->setRouteClass(DashedRoute::class);
 
@@ -62,9 +64,11 @@ $routes->scope('/', function (RouteBuilder $builder) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, templates/Pages/home.php)...
      */
-    $builder->connect('/',
+    $builder->connect(
+        '/',
         ['controller' => 'Users', 'action' => 'login', 'home'],
-        ['_name' => 'login']);
+        ['_name' => 'login']
+    );
 
     /*
      * ...and connect the rest of 'Pages' controller's URLs.
