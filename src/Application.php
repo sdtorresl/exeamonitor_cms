33b18e3fa4d053b607a55e3cda6d4e7cd22122f3
Sergio Torres <sdtorresl@unal.edu.co>
2023-02-06 23:03:58 -0500

    Allow CORS

--- src/Application.php
+++ src/Application.php
@@ -126,21 +126,21 @@ class Application extends BaseApplication implements AuthenticationServiceProvid
             return $handler->handle($request);
         });
 
-        if (Configure::read('debug')) {
-            // Add CORS for development environments.
-            $middlewareQueue
-                ->add(function ($request, $handler) {
-                    $response = $handler->handle($request)
-                        ->withHeader('Access-Control-Allow-Origin', '*')
-                        ->withHeader('Access-Control-Allow-Methods', '*')
-                        ->withHeader('Access-Control-Allow-Credentials', 'true')
-                        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With')
-                        ->withHeader('Access-Control-Allow-Headers', 'Content-Type')
-                        ->withHeader('Access-Control-Allow-Type', 'application/json');
+        //if (Configure::read('debug')) {
+        // Add CORS for development environments.
+        $middlewareQueue
+            ->add(function ($request, $handler) {
+                $response = $handler->handle($request)
+                    ->withHeader('Access-Control-Allow-Origin', '*')
+                    ->withHeader('Access-Control-Allow-Methods', '*')
+                    ->withHeader('Access-Control-Allow-Credentials', 'true')
+                    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With')
+                    ->withHeader('Access-Control-Allow-Headers', 'Content-Type')
+                    ->withHeader('Access-Control-Allow-Type', 'application/json');
 
-                    return $response;
-                });
-        }
+                return $response;
+            });
+        //}
         return $middlewareQueue;
     }
 
@@ -175,7 +175,6 @@ class Application extends BaseApplication implements AuthenticationServiceProvid
                 'returnPayload' => false
             ]);
             $authenticationService->loadAuthenticator('Authentication.Session');
-
         } else {
             $authenticationService = new AuthenticationService([
                 'unauthenticatedRedirect' => Router::url('/', false),
