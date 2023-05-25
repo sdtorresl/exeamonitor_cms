6b67819bd8ac51b9a905260212f482c448007573
Andres Culma <aculma@seedem.co>
2023-05-25 12:10:51 -0500

    update

--- config/Migrations/20230518084420_AddRulesHours.php
+++ config/Migrations/20230518084420_AddRulesHours.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Migrations\AbstractMigration;
 
-class AddPlaybookIdToPointsOfSale extends AbstractMigration
+class AddRulesHours extends AbstractMigration
 {
     /**
      * Change Method.
